55072d241254b95991853785cca0d4813d5888a6
Luwengier <g217881111@gmail.com>
2021-10-28 14:23:39 +0800

    Build basic websocket project

---
+++ nativeWs/justWsServer.js
@@ -0,0 +1,25 @@
+const http = require('http')
+// 3rd part module ws!
+const websocket = require('ws')
+
+const server = http.createServer((req, res) => {
+  res.end("I am connected !")
+})
+
+const wss = new websocket.Server({ server })
+wss.on('headers', (headers, req) => {
+  console.log(headers)
+})
+
+wss.on('connection', (ws, req) => {
+  ws.send('Welcome to the websocket server!!')
+  ws.on('message', (message) => {
+    console.log(`${message}`);
+  });
+  // ws.onmessage = (msg) => {
+  //   console.log(msg)
+  // }
+})
+
+
+server.listen(8000)
